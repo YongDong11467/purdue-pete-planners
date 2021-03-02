2e7a8b2c55d265266f39076f457240ff8bbfab5f
Marcel Goodwin <mp.goodwin98@gmail.com>
2021-03-01 23:25:14 -0500

    added routing for account api

--- server/api/account.js
+++ server/api/account.js
@@ -1,8 +1,18 @@
 const router = require("express").Router();
+const account_manager = require("../account_manager");
 
 router.route("/").get((req, res) => { 
   const account = ["login", "register", "profile"]
-  res.json(dining);
+  res.json(account);
+});
+
+router.get("/login", (req,res) => {
+  
+  res.send('200: success');
+});
+
+router.get("/register", (req,res) => {
+    
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const diningRouter = require("./api/dining");
+const accountRouter = require("./api/account");
 const account_manager = require("./account_manager");
 
 
@@ -11,9 +12,11 @@ const app = express(),
 async function initialize_app(){
     await account_manager.startDatabaseConnection();
 }
+
+initialize_app();
 app.use(bodyParser.json());
 app.use("/api/dining", diningRouter);
-
+app.use("/api/account", accountRouter);
 
 app.get('/', (req,res) => {
     res.send('Default route');
