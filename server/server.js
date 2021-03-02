603762185e3710053530dd3c7e7087e53d3c83af
Marcel Goodwin <mp.goodwin98@gmail.com>
2021-03-01 19:51:52 -0500

    connected mongodb

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const diningRouter = require("./api/dining");
-const accountManager = require("./account_manager");
+const account_manager = require("./account_manager");
 
 
 const app = express(),
       bodyParser = require("body-parser");
       port = 3080;
 
+
+async function initialize_app(){
+    await account_manager.startDatabaseConnection();
+}
 app.use(bodyParser.json());
 app.use("/api/dining", diningRouter);
 
