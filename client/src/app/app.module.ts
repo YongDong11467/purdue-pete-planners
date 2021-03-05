64e3da79af5c9fea6884e64f5248135afccd37d8
YongDong11467 <yongdong11467@gmail.com>
2021-03-04 19:59:35 -0500

    redownloaded modules

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
