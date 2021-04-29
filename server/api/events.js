20dee27d1a52ba20bd1df91f655dd74be2f2e681
nalitman <nalitman17@gmail.com>
2021-04-29 18:54:59 -0400

    Event Invitations fully functional
    -Accept/Decline invitation
    -Delete events
    -Remove from interested tab
    -Refreshing page

--- server/api/events.js
+++ server/api/events.js
@@ -7,20 +7,23 @@ router.route("/").get((req, res) => {
 });
 
 router.post("/events", (req,res) => {
-
     res.send('200: success')
 });
 
 router.route("/createEvent").post((req, res) => {
-  console.log(req);
    return manager.createEvent(req.body.name, req.body.description, req.body.Time, req.body.link, req.body.location, req.body.repeat, req.body.owner)
     .then(success => res.status(200).json(success))
     .catch(err => res.status(400).json(err));
   });
 
+  router.route("/searchInvitedEvent").get((req, res) => {
+    manager.searchInvitedEvent(req.query.prefix).then(users => {
+      res.json(users);
+    });
+  });
+
   router.route("/searchUserEvent").get((req, res) => {
     manager.searchUserEvent(req.query.prefix).then(users => {
-      //console.log(users)
       res.json(users);
     });
   });
@@ -45,11 +48,40 @@ router.route("/createEvent").post((req, res) => {
   });
 
   router.route("/updateEventInvite").post((req, res) => {
-    console.log("events hello");
-    console.log("Hello from events.js ", req.body.reciever, req.body.sender, req.body.eventName, req.body.eventID);
     return manager.updateEventInvite(req.body.reciever, req.body.sender, req.body.eventName, req.body.eventID)
     .then(success => res.status(200).json(success))
     .catch(err => res.status(400).json(err));
+  });
+
+  router.route("/searchUserEventInvites").get((req,res) =>{
+    manager.searchUserEventInvites(req.query.prefix).then(response => {
+      console.log(response);
+      res.json(response);
+    });
+  });
+
+  router.route("/acceptEventInvite").post((req,res) => {
+    return manager.acceptEventInvite(req.body.params.sentUser, req.body.params.sentName, req.body.params.event_id, req.body.params.user)
+    .then(success => res.status(200).json(success))
+    .catch(err => res.status(400).json(err));
+  });
+
+  router.route("/declineEventInvite").post((req,res) => {
+    return manager.declineEventInvite(req.body.params.sentUser, req.body.params.sentName, req.body.params.event_id, req.body.params.user)
+    .then(success => res.status(200).json(success))
+    .catch(err => res.status(400).json(err));
+  });
+
+  router.route("/deleteEvent").post((req,res) => {
+    return manager.deleteEvent(req.body.params.user_delete, req.body.params.event_name, req.body.params.event_id)
+    .then(success => res.status(200).json(success))
+    .catch(err => res.status(400).json(err));
+  });
+
+  router.route("/removeEvent").post((req,res) => {
+    return manager.removeEvent(req.body.params.user_remove, req.body.params.event_id)
+    .then(success => res.status(200).json(success))
+    .catch(err => res.status(400).json(err))
   });
 
   module.exports = router;
