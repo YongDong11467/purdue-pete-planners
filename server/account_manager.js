778c163fc7f9ae6be720cfd03e15c4aff47d55c0
Luke Gregurich <lgreguri@purdue.edu>
2021-04-30 10:37:46 -0400

    comma fix

--- server/account_manager.js
+++ server/account_manager.js
@@ -81,8 +81,7 @@ const createAccount = async function(username, email, major, pass, phone, addres
 		"friend":[],
 		"friend_request":[],
 		"book_room":[],
-		"classes":[]
-
+		"classes":[],
 		"event_invite":[],
     "class_list":[]
 	}
